d3a0a0a56c8654dbd5766f9808a3b0279ecd1e0d
Soon Leqi <145783043+AlphaDesk@users.noreply.github.com>
2024-09-16 22:52:23 +0800

    Initial Commit

---
+++ BeaverApp/BeaverAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaverAppApp.swift
+//  BeaverApp
+//
+//  Created by Leqi Soon on 16/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BeaverAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
